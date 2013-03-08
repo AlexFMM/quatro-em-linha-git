6e1c79902dacf535c8a2da16aec02cadc31b64c5
AlexFMM <alex.f.m.martins@gmail.com>
2013-03-08 20:31:35 +0000

    v0.2 missing check wins

--- quatro-em-linha/src/com/me/quatro_em_linha/Ficha.java
+++ quatro-em-linha/src/com/me/quatro_em_linha/Ficha.java
@@ -1,10 +1,17 @@
 package com.me.quatro_em_linha;
 
+import com.badlogic.gdx.graphics.Color;
+
 public class Ficha {
 	int jogador;
+	Color color;
 	
 	Ficha(int a){
 		jogador=a;
+		if(a==0)
+			color = new Color(1,1,1,1);
+		else
+			color = new Color(0.5f,0.5f,1,1);
 	}
 
 }
--- quatro-em-linha/src/com/me/quatro_em_linha/Tabela.java
+++ quatro-em-linha/src/com/me/quatro_em_linha/Tabela.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 
 public class Tabela implements Screen {
 
@@ -11,11 +13,24 @@ public class Tabela implements Screen {
 	MainGame maingame;
 	int player_actual;
 	float w, h;
+	Vector3 mpos = new Vector3(0, 0, 0);
+	//Vector2 pos[][];
 	
 	Tabela(MainGame n){
 		tab = new Ficha[6][7];
 		maingame = n;
 		player_actual=0;
+		make_null();
+		
+		/*pos = new Vector2[1][7];
+		pos[0][0] = new Vector2(Gdx.graphics.getWidth()/7, 1); 
+		pos[0][1] = new Vector2(2*Gdx.graphics.getWidth()/7, 1);
+		pos[0][2] = new Vector2(3*Gdx.graphics.getWidth()/7, 1);
+		pos[0][3] = new Vector2(4*Gdx.graphics.getWidth()/7, 1);
+		pos[0][4] = new Vector2(5*Gdx.graphics.getWidth()/7, 1);
+		pos[0][5] = new Vector2(6*Gdx.graphics.getWidth()/7, 1);
+		pos[0][6] = new Vector2(Gdx.graphics.getWidth(), 1);*/
+		
 	}
 	
 	@Override
@@ -23,8 +38,15 @@ public class Tabela implements Screen {
 		// TODO Auto-generated method stub
 		Gdx.gl.glClearColor(1, 0.8f, 0, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		mpos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+		maingame.camera.unproject(mpos);
 		w = Gdx.graphics.getWidth();
 		h = Gdx.graphics.getHeight();
+		
+		if(Gdx.input.justTouched()){
+			Gdx.app.log("posx", " "+Gdx.input.getX());
+			Gdx.app.log("posy", " "+Gdx.input.getY()+"\n");
+		}
 		
 		for(int i=1; i<7; i++){
 			MainGame.renderer.begin(ShapeType.Line);
@@ -38,6 +60,20 @@ public class Tabela implements Screen {
 			MainGame.renderer.end();
 		}
 		
+		if(Gdx.input.justTouched()){
+			if((Gdx.input.getX()<w/7)&&(Gdx.input.getX()>0)){nova_ficha(0);}
+			else if((Gdx.input.getX()<2*w/7)&&(Gdx.input.getX()>w/7)){nova_ficha(1);}
+			else if((Gdx.input.getX()<3*w/7)&&(Gdx.input.getX()>2*w/7)){nova_ficha(2);}
+			else if((Gdx.input.getX()<4*w/7)&&(Gdx.input.getX()>3*w/7)){nova_ficha(3);}
+			else if((Gdx.input.getX()<5*w/7)&&(Gdx.input.getX()>4*w/7)){nova_ficha(4);}
+			else if((Gdx.input.getX()<6*w/7)&&(Gdx.input.getX()>5*w/7)){nova_ficha(5);}
+			else if((Gdx.input.getX()<w)){nova_ficha(6);}
+			else {Gdx.app.log("DAFUK:", "YOU DIDNT SEE NOTHING!");}
+		}
+		
+		draw_ficha();
+		
+		
 	}
 	
 	void change_player(){
@@ -50,11 +86,32 @@ public class Tabela implements Screen {
 	}
 	
 	void nova_ficha(int x){
-		for(int i=5;i>0;i--){
+		for(int i=5;i>=0;i--){
 			if(tab[i][x]==null){
 				tab[i][x]= new Ficha(player_actual);
 				change_player();
 				break;
+			}
+		}
+	}
+	
+	void make_null(){
+		for(int i=0;i<6;i++){
+			for(int j=0;j<7;j++){
+				tab[i][j]=null;
+			}
+		}
+	}
+	
+	void draw_ficha(){
+		for(int i=0;i<6;i++){
+			for(int j=0;j<7;j++){
+				if(tab[i][j]!=null){
+					MainGame.renderer.begin(ShapeType.FilledCircle);
+					MainGame.renderer.setColor(tab[i][j].color);
+					MainGame.renderer.filledCircle((j*w/7)+w/14, (h-(i+1)*h/6)+h/12, 20);
+					MainGame.renderer.end();
+				}
 			}
 		}
 	}
